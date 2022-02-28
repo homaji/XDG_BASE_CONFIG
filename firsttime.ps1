97b3f6af87c08d45858118d04d1d584716a7198b
homaji <homaji@gmail.com>
2022-03-01 08:15:10 +0900

    [firsttime.ps1] Set Environment
    Create junction folder
    Set user environmet XDG Base Directories.

--- firsttime.ps1
+++ firsttime.ps1
@@ -1,13 +1,8 @@
-$env:HOME=$env:USERPROFILE
+$env:HOME=$env:HOMEPATH
 $env:XDG_CONFIG_HOME="$env:HOMEPATH\.config"
-
-
 New-Item $env:XDG_CONFIG_HOME -ItemType Directory
 
-# $DATA_FROM=$env:OneDrive + "\Software\Settings\nvim\"
-$DATA_FROM=$PSScriptRoot
-$DATA_TO=$env:XDG_CONFIG_HOME + "\nvim\"
-cmd /c mklink /j $DATA_TO $DATA_FROM
+cmd /c mklink /j  "$env:XDG_CONFIG_HOME\nvim" "$env:OneDrive\Software\Settings\nvim\"
 
 $env:XDG_CACHE_HOME="$env:TEMP\neovim\"
 New-Item $env:XDG_CACHE_HOME -ItemType Directory
@@ -15,6 +10,11 @@ New-Item $env:XDG_CACHE_HOME -ItemType Directory
 $env:XDG_DATA_HOME="$env:HOMEPATH\.local\"
 New-Item $env:XDG_DATA_HOME -ItemType Directory 
 
+#Set permanent env
+[Environment]::SetEnvironmentVariable('XDG_CONFIG_HOME',$Env:XDG_CONFIG_HOME,'User')
+[Environment]::SetEnvironmentVariable('XDG_CACHE_HOME',$Env:XDG_CACHE_HOME,'User')
+[Environment]::SetEnvironmentVariable('XDG_DATA_HOME',$Env:XDG_DATA_HOME,'User')
 
 iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
     ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
+
