3db5acb1b7ce49ab6b8fc850d47d5dd68609184b
homaji <homaji@gmail.com>
2022-05-15 20:27:19 +0900

    [lua]Separete config file.
    make lua directory. And create 001-core.lua file.

--- init.lua
+++ init.lua
@@ -1,7 +1,3 @@
 -- luaで作成
 
--- 番号表示
-vim.opt.number = true
-
--- クリップボード連携
-opt.clipboard:append({unnamedeplus = true})
+require('001-core')
---
+++ lua/001-core.lua
@@ -0,0 +1,6 @@
+
+-- 番号表示
+vim.opt.number = true
+
+-- クリップボード連携
+opt.clipboard:append({unnamedeplus = true})
