6d27e597f83c4a7fcd4469d0e4d428a3889c59c1
homaji <homaji@gmail.com>
2022-06-11 19:46:15 +0900

    [keymap]Return to vim.keymap.set
    vim.api.nvim_set_keymap is old way.

--- lua/002_keymap.lua
+++ lua/002_keymap.lua
@@ -1,23 +1,23 @@
--- vim.api.nvim_set_keymap(mode, lhs, rhs, options,{noremap = true})
+-- vim.keymap.set(mode, lhs, rhs, options,{noremap = true})
 
 -- set s key to Window moving.
-vim.api.nvim_set_keymap('n', 's', '<NOP>',{noremap = true})
+vim.keymap.set('n', 's', '<NOP>',{noremap = true})
 -- set s+hjkl move to other window
-vim.api.nvim_set_keymap('n', 'sj', '<C-w>j',{noremap = true})
-vim.api.nvim_set_keymap('n', 'sk', '<C-w>k',{noremap = true})
-vim.api.nvim_set_keymap('n', 'sl', '<C-w>l',{noremap = true})
-vim.api.nvim_set_keymap('n', 'sh', '<C-w>h',{noremap = true})
+vim.keymap.set('n', 'sj', '<C-w>j',{noremap = true})
+vim.keymap.set('n', 'sk', '<C-w>k',{noremap = true})
+vim.keymap.set('n', 'sl', '<C-w>l',{noremap = true})
+vim.keymap.set('n', 'sh', '<C-w>h',{noremap = true})
 -- set s+sv separate window
-vim.api.nvim_set_keymap('n', 'ss', '<C-w>s',{noremap = true})
-vim.api.nvim_set_keymap('n', 'sv', '<C-w>v',{noremap = true})
+vim.keymap.set('n', 'ss', '<C-w>s',{noremap = true})
+vim.keymap.set('n', 'sv', '<C-w>v',{noremap = true})
 -- set s+nptT move tab
-vim.api.nvim_set_keymap('n', 'sn', 'gt',{noremap = true})
-vim.api.nvim_set_keymap('n', 'sp', 'gT',{noremap = true})
-vim.api.nvim_set_keymap('n', 'st', ':<C-u>tabnew<CR>',{noremap = true})
+vim.keymap.set('n', 'sn', 'gt',{noremap = true})
+vim.keymap.set('n', 'sp', 'gT',{noremap = true})
+vim.keymap.set('n', 'st', ':<C-u>tabnew<CR>',{noremap = true})
 -- set s+qQ kill buffer or close window
-vim.api.nvim_set_keymap('n', 'sq', ':<C-u>q<CR>',{noremap = true})
-vim.api.nvim_set_keymap('n', 'sb', ':<C-u>bd!<CR>',{noremap = true})
+vim.keymap.set('n', 'sq', ':<C-u>q<CR>',{noremap = true})
+vim.keymap.set('n', 'sb', ':<C-u>bd!<CR>',{noremap = true})
 
 -- set ESC+ESC Clear Search hilight
-vim.api.nvim_set_keymap('n', '<ESC><ESC>', ':<C-u>set nohlsearch!<CR>',{noremap = true})
+vim.keymap.set('n', '<ESC><ESC>', ':<C-u>set nohlsearch!<CR>',{noremap = true})
 
