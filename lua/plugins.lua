d6ac27c4bb5d04f03d379e0855ad09d155662950
homaji <homaji@gmail.com>
2022-06-21 08:10:09 +0900

    [neogit]Install Plugin.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,15 +2,16 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function()
     
-   use{ 'nvim-lua/plenary.nvim'}
+   use{ 'nvim-lua/plenary.nvim' }
    use{ 'kyazdani42/nvim-web-devicons', opt = true}
    use{ 'wbthomason/packer.nvim', opt = true}
-   use{ 'nvim-telescope/telescope.nvim'}
-   use{ 'nvim-lualine/lualine.nvim'}
-   use{'tkmpypy/chowcho.nvim'}
-   use{'rebelot/kanagawa.nvim'}
-   use{'nathom/filetype.nvim'}
-   use{'obaland/vfiler.vim'}
-   use{'goolord/alpha-nvim'}
+   use{ 'nvim-telescope/telescope.nvim' }
+   use{ 'nvim-lualine/lualine.nvim' }
+   use{ 'tkmpypy/chowcho.nvim' }
+   use{ 'rebelot/kanagawa.nvim' }
+   use{ 'nathom/filetype.nvim' }
+   use{ 'obaland/vfiler.vim' }
+   use{ 'goolord/alpha-nvim' }
+   use{ 'TimUntersberger/neogit' }
 end)
 
