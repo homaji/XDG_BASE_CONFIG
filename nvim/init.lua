b14c0e11d4697a663a71fc9446736423056df7ee
homaji <homaji@gmail.com>
2022-08-26 23:30:14 +0000

    [toggleterm]Add Plugin Toggleterm
    Open terminal with <C-\> in float window.

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,5 +7,6 @@ require('101-lualine')
 require('102-chowcho')
 require('103-changelog')
 require('104-alpha')
+require('105-toggleterm')
 
 vim.cmd('cd ~')
--- nvim/lua/105-toggleterm.lua
+++ nvim/lua/105-toggleterm.lua
@@ -1,7 +1,22 @@
-
 local status_ok, toggleterm = pcall(require, "toggleterm")
 if not status_ok then
     return
 end
 
-
+require("toggleterm").setup{
+    function(term)
+        if term.direction == "horizontal" then
+            return 15
+        elseif term.direction == "vertical" then
+            return vim.o.columns * 0.4
+        end
+    end,
+    -- set up keymaps
+    open_mapping = [[<c-\>]],
+    -- open type
+    direction = 'float', -- vertical | horizontal | tab | float
+    -- option of float window
+    float_opts ={
+        border = 'curved' -- single | double | shadow | curve
+    },
+}
