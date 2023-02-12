0c53f955eec25cd9efab4365d7179a953036c47f
homaji <homaji@gmail.com>
2023-02-12 16:31:18 +0900

    [wezterm]Use Opacity.
    Set Window BackGroundOpacity to 0.8. And Terminal Window is TransParent.
    Set Text BackGroundOpacity to 0.8. And neovim with Wezterm is
    TransParent.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,9 +1,11 @@
 local wezterm = require 'wezterm';
 
 return {
-	default_prog = {'pwsh.exe'},
-	font = wezterm.font("Cica"),
-	use_ime = true,
-	font_size = 14.0,
-	color_scheme = "kanagawa (Gogh)",
+    default_prog = {'pwsh.exe'},
+    font = wezterm.font("Cica"),
+    use_ime = true,
+    font_size = 14.0,
+    color_scheme = "kanagawa (Gogh)",
+    window_background_opacity = 0.8,
+    text_background_opacity = 0.7,
 }
