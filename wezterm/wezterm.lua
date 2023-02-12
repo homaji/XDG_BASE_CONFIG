cd8cb6527df90b75c03d130f160210f1c03d3b2f
homaji <homaji@gmail.com>
2023-02-12 20:45:28 +0900

    [wezterm]Headlessmode
    Use Configure "window_decolatoins"

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,9 +3,10 @@ local wezterm = require 'wezterm';
 return {
     default_prog = {'pwsh.exe'},
     font = wezterm.font("Cica"),
-    use_ime = true,
     font_size = 14.0,
+    use_ime = true,
     color_scheme = "kanagawa (Gogh)",
     window_background_opacity = 0.8,
     text_background_opacity = 0.7,
+    window_decorations = "RESIZE",
 }
