d2cad64dce890d589d815c68b54d3bf221c1ba13
homaji <homaji@gmail.com>
2023-11-07 19:27:47 +0900

    [nvim]setting lualine.
    Change lualine.lua. Added initialize lualine and end.

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,3 +1,31 @@
 return{
-    'nvim-lualine/lualine.nvim'
+    'nvim-lualine/lualine.nvim',
+    event = { "BufReadPre", "BufNewFile"},
+    config = function()
+        local lualine = require('lualine')
+
+        local config = {
+            options = {
+                icons_enabled = true,
+                theme = 'auto',
+                component_separators = { left = '', right = ''},
+                section_separators = { left = '', right = ''},
+                disabled_filetypes = {},
+                always_divide_middle = true,
+                globalstatus = true,
+            },
+            sections = {
+                lualine_a = {'mode'},
+                lualine_b = {'branch', 'diff', 'diagnostics'},
+                lualine_c = {'filename'},
+                lualine_x = {'encoding', 'fileformat', 'filetype'},
+                lualine_y = {'progress'},
+                lualine_z = {'location'},
+            },
+            tabline = {},
+            extensions = {},
+        }
+    -- don't forget to initialize lualine
+    lualine.setup(config)
+    end
 }
