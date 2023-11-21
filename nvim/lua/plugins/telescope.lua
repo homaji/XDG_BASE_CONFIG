54e51a52f546db4d4bc9047a39149a3015e70eda
homaji <homaji@gmail.com>
2023-11-21 19:30:01 +0900

    [nvim.telescope]Added Config
    Change Config from youtube video.
    Added keymap Using C-k and C-j and C-q

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,9 +2,29 @@ return{
     'nvim-telescope/telescope.nvim',
     dependencies = {
         'nvim-lua/plenary.nvim', 
+        'BurntSushi/ripgrep',
+        'nvim-telescope/telescope-media-files.nvim',
     },
     keys ={
         {'sf', ':Telescope find_files<CR>'},
     },
+      config = function()
+          local telescope = require("telescope")
+          local actions = require("telescope.actions")
 
+          telescope.setup({
+              defaults = {
+                  mappings = {
+                      i = {
+                          ["<C-k>"] = actions.move_selection_previous, --move to prev result
+                          ["<C-j>"] = actions.move_selection_next, --move tonext result
+                          ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                      }
+                  }
+              }
+          })
+
+          telescope.load_extension("media_files");
+
+      end
 }
